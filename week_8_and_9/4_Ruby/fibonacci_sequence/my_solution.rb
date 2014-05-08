f4abecbc3b327c069ad3cc4410f4ac7a006238cd
Kim Girard <kimberley.girard@gmail.com>
2014-05-08 16:56:03 -0400

    Solution to Fibonacci; not 100% complete

--- week_8_and_9/4_Ruby/fibonacci_sequence/my_solution.rb
+++ week_8_and_9/4_Ruby/fibonacci_sequence/my_solution.rb
@@ -1,16 +1,51 @@
 # U3.W8-9: 
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Ahmed Al-Bahar.
 
 # 2. Pseudocode
+=begin
+I'm going to build a loop that calculates the next number in the fibonacci sequence until the number entered in #is_fibonacci? is less than the current fibonacci number I've calculated.
 
+DEFINE method called is_fibonacci? that takes num as an argument
+	SET variable x equal to 0, the first number of fibonacci
+	SET variable y equal to 1, the second number of fibonacci
+	WHILE x is less than or equal to num
+		IF num is equal to x, return true
+		ELSE IF num is less than x, return false
+		ELSE SET x equal to x plus y
+			SET y equal to x
+	END WHILE
+END method
 
+=end
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 
+def fib(n)
+  return n if (0..1).include? n
+  fib(n-1) + fib(n-2) if n > 1
+end
 
 # 3. Initial Solution
 
 def is_fibonacci?(num)
+	x = 0
+	y = 1
+	z = 1	
+	fib = [0,1,1]
 
+	while num >= z
+		z = y + z # 2 : 3 : 5
+		y = z - y # 1 : 2 : 3
+		x = z - y # 1 : 1 : 2
+		fib.push(z)
+
+		if fib.include? num 
+			return true
+		elsif num < z
+			return false
+		end
+	end
 end
 
 
